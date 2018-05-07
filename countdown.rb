a6f949ac57d62d6e04bc54b051f78a70e2251707
Michael Ballard <ballard.michael9@gmail.com>
2018-05-07 12:56:54 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,6 +9,13 @@ def countdown(num)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
+def countdown_with_sleep(num)
+    counter = num
+  while counter != 0 
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
   
 end

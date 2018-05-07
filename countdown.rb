b3cb46a973f345edc3f29d310811b6aaf9be32eb
Michael Ballard <ballard.michael9@gmail.com>
2018-05-07 12:48:34 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,10 +2,9 @@
 
 def countdown(num)
   counter = num
-  
   while counter != 0 
-  puts "#{counter} SECOND(S)!"
-  counter -= 1
-end
-puts "HAPPY NEW YEAR!"
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+  puts "HAPPY NEW YEAR!"
 end
